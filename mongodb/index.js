e354318fbf7badc9882607161a5c7c9f9aa02bb2
Jass1321 <jassyra13216@gmail.com>
2021-11-16 12:23:24 -0500

    mongodb micro

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,12 +1,12 @@
 import express from "express";
-
+import mongoRouter from "./network";
 import { database_port_microservice } from "../config/config";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hola base de datos");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoRouter);
 
 //* database_port_microservice : 6601
 app.listen(database_port_microservice, () =>
